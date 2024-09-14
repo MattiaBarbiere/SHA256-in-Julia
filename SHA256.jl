62ddb18dc0bea62e9fe0e65fb73fe60dbcaaed90
MattiaBarbiere <barbiere.mattia@gmail.com>
2024-09-14 17:57:41 +0200

    Coded the preprossesing part

--- SHA256.jl
+++ SHA256.jl
@@ -1,8 +1,4 @@
-#Note: In this project hex and decimal numbers will be used
-# as strings
-char = 'a'
-ascii_code = Int(char)
-
+#Note: In this project hex and decimal numbers will be used as strings
 
 #####################Functions######################
 
@@ -40,11 +36,69 @@ end
 # g = binary_to_decimal(f)
 # println(g)
 
-function word_to_binary(word::String)
-    result = ""
+#A single word into binary
+function word_to_binary(word)
+    result = raw""
     for i in word
-        result *= decimal_to_binary(Int(i)) * " "
+        result *= decimal_to_binary(Int(i))
     end 
     return result   
 end
 
+#The whole sentence into binary
+function sentence_to_binary(sentence)
+    result = raw""
+    for i in split(sentence, " ")
+        result *= word_to_binary(i)
+    end
+    return result
+end
+
+#A function that adds spaces every 8 bits (this is only for visual purposes)
+function add_space(bit_string)
+    #Make sure there are no spaces already in the bit string
+    bit_string = replace(bit_string, " " => "")
+
+    #Transform the string to an array (so we can add element)
+    array = split(bit_string, "")
+    i = 9
+    while i <= length(array)
+        insert!(array, i, " ")
+        i += 9
+    end
+    return join(array)
+end
+
+
+############Preprossesing functions###################
+message = "RedBlockBlue"
+function padding(bit_string)
+    #I remove any spaces in the bit_string so that we have strings of the correct dimension
+    bit_string = replace(bit_string, " " => "")
+
+    #Calculate the length
+    len = length(bit_string)
+    
+    #The next multiple of 512 after adding the extra 64+1 bits
+    multiple = div(len + 65, 512) + 1
+
+    #Calculating the k
+    k = 512 * multiple - len - 65
+
+    #Adding the "1" bit
+    bit_string *= "1"
+
+    #Adding the extra padding
+    bit_string = rpad(bit_string, len + 1 + k, "0")
+    
+    #The length of the sentence written with 64 bits
+    len_in_binary = string(len, base = 2, pad=64)
+
+    #Final concatination
+    bit_string *= len_in_binary
+
+    return bit_string
+end
+
+c = padding(sentence_to_binary(message))
+println(add_space(c))
